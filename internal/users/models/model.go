a08e69b47e46d06e319e8b39c54e2e4152120f8a
GaijinZ <tomasz.mostowy@gmail.com>
2023-06-21 00:02:54 +0200

    omitempty password field

--- internal/users/models/model.go
+++ internal/users/models/model.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	ID int `json:"id" form:"id"`
+	ID        int    `json:"id" form:"id"`
 	Firstname string `json:"firstname" form:"firstname"`
-	Lastname string `json:"lastname" form:"lastname"`
-	Email string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-	Role string `json:"role" form:"role"`
+	Lastname  string `json:"lastname" form:"lastname"`
+	Email     string `json:"email" form:"email"`
+	Password  string `json:"password,omitempty" form:"password"`
+	Role      string `json:"role" form:"role"`
 }
