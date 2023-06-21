25bf904ddfd29257a5bea25a44187df439e94a9f
GaijinZ <tomasz.mostowy@gmail.com>
2023-06-21 14:16:30 +0200

    fixed saving env variables

--- init-scripts/env-vars.sh
+++ init-scripts/env-vars.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-SOURCE_FILE="/home/vagrant/.bash_profile"
+SOURCE_FILE="$HOME/.bash_profile"
 
 touch "$SOURCE_FILE"
 
-echo "USERS_SERVER_PORT=":5000"" >> "$SOURCE_FILE"
-echo "POSTGRES_USERS="postgres://tmosto:tmosto@localhost:5432/usersdb"" >> "$SOURCE_FILE"
-echo "GOPATH=$HOME/go" >> "$SOURCE_FILE"
-echo "PATH=$PATH:/usr/local/go/bin" >> "$SOURCE_FILE"
+sudo chmod 777 "$SOURCE_FILE"
 
-# echo "All variables has been saved in: $SOURCE_FILE"
+echo "export USERS_SERVER_PORT=:5000" > "$SOURCE_FILE"
+echo "export POSTGRES_USERS=postgres://tmosto:tmosto@localhost:5432/usersdb" >> "$SOURCE_FILE"
+echo "export GOPATH=$HOME/go" >> "$SOURCE_FILE"
+echo "export PATH=$PATH:/usr/local/go/bin" >> "$SOURCE_FILE"
+
+echo "All variables has been saved in: $SOURCE_FILE"
 
 source $SOURCE_FILE
