dbc1d13770843b177a08eb24dbb4b7c5c350c9f5
GaijinZ <tomasz.mostowy@gmail.com>
2023-06-21 23:58:52 +0200

    password required model field

--- internal/users/models/model.go
+++ internal/users/models/model.go
@@ -2,11 +2,11 @@ package models
 
 type User struct {
 	ID        int    `json:"id" form:"id"`
-	Firstname string `json:"firstname" form:"firstname"`
-	Lastname  string `json:"lastname" form:"lastname"`
+	Firstname string `json:"firstname,omitempty" form:"firstname"`
+	Lastname  string `json:"lastname,omitempty" form:"lastname"`
 	Email     string `json:"email" form:"email" validate:"required,email"`
-	Password  string `json:"password,omitempty" form:"password"`
-	Role      string `json:"role" form:"role"`
+	Password  string `json:"password,omitempty" form:"password" binding:"required"`
+	Role      string `json:"role,omitempty" form:"role"`
 }
 
 type Authentication struct {
